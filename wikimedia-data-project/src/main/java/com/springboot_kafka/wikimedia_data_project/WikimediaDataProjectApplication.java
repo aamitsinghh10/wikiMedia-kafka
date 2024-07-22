16ae274342514d1f656973746d7119fc7ba25c20
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2024-07-22 16:54:16 +0530

    Add files via upload

---
+++ wikimedia-data-project/src/main/java/com/springboot_kafka/wikimedia_data_project/WikimediaDataProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_kafka.wikimedia_data_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaDataProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaDataProjectApplication.class, args);
+	}
+
+}
---
+++ wikimedia-data-project/src/test/java/com/springboot_kafka/wikimedia_data_project/WikimediaDataProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_kafka.wikimedia_data_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaDataProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
